d42836a78051eb6396559219b4456b0bb0b2fa1f
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-11-08 13:06:36 +0530

    1.Border stroke color and stroke width is applied 2.Padding is adjusted 3.Offset is added i.e place the content along the xy axis with parent

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,12 +23,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Yellow),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
+                    .background(Color.LightGray)
+                    .fillMaxWidth()
+                    .border(2.dp, Color.Magenta)
+                    .padding(20.dp)
             ) {
-                Text("Hello")
+                Text("Hello",Modifier.offset((-50).dp, (80).dp))
                 Text("World")
                 Text("Hello")
                 Text("World")
